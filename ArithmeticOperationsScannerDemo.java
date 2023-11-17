c0181d9a0fe8fd05d9f2e1d2f1621ba1bdf8cf25
RK Thilak <108559344+RKThilak@users.noreply.github.com>
2023-11-17 19:14:00 +0530

    ArithmeticOperationsScannerDemo.java

--- ArithmeticOperationsScannerDemo.java
+++ ArithmeticOperationsScannerDemo.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ArithmeticOperationsScannerDemo {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a integer number a=");
 		int a=sc.nextInt();
-		System.out.println("Enter a integer number a="+a);
 		System.out.println();
+		System.out.println("Enter a integer number b=");
 		int b=sc.nextInt();
-		System.out.println("Enter a integer number b="+b);
 		int sum=a+b;
 		int sub=a-b;
 		int mul=a*b;
